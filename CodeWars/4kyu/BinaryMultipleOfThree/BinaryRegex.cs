e46d85d76eb8c3a8702e466e563fd837a72a0644
Tsypin Ilya <ilyaboy@list.ru>
2022-05-10 12:17:07 +0300

    Init kata BinaryMultipleOfThree(4kyu)

---
+++ CodeWars/4kyu/BinaryMultipleOfThree/BinaryRegex.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeWars._4kyu.BinaryMultipleOfThree;
+
+public class BinaryRegex {
+
+    public static Regex MultipleOf3() {
+        // Regular expression that matches binary inputs that are multiple of 3
+        return new Regex("");
+    }
+}
