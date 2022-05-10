af6cb2d639259acd52e57685c92c468f69d5e761
Tsypin Ilya <ilyaboy@list.ru>
2022-05-10 12:06:48 +0300

    Add solution for CountingChangeCombinations(4kyu)

--- CodeWars/4kyu/CountingChangeCombinations/Kata.cs
+++ CodeWars/4kyu/CountingChangeCombinations/Kata.cs
@@ -2,9 +2,24 @@
 
 public static class Kata
 {
-    public static int CountCombinations(int money, int[] coins)
+    public static int CountCombinations(int money, int[] coins) =>
+        FindCombination(money, coins, 0);
+
+    private static int FindCombination(int currentMoney, int[] coins, int lenght)
     {
-        // TODO
-        return 0;
+        if (currentMoney == 0)
+        {
+            return 1;
+        }
+
+        if (currentMoney < 0 || lenght == coins.Length)
+        {
+            return 0;
+        }
+
+        var withFirstCoin = FindCombination(currentMoney - coins[lenght], coins, lenght);
+        var withoutFirstCoin = FindCombination(currentMoney, coins, lenght + 1);
+        
+        return withFirstCoin + withoutFirstCoin;
     }
 }
