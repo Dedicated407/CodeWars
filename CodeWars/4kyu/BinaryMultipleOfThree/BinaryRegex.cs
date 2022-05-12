27475f934934ac01c69d4b8bd835a5986204e45d
Tsypin Ilya <ilyaboy@list.ru>
2022-05-12 13:52:35 +0300

    Add solution for BinaryMultipleOfThree(4kyu)

--- CodeWars/4kyu/BinaryMultipleOfThree/BinaryRegex.cs
+++ CodeWars/4kyu/BinaryMultipleOfThree/BinaryRegex.cs
@@ -4,8 +4,15 @@ namespace CodeWars._4kyu.BinaryMultipleOfThree;
 
 public class BinaryRegex {
 
+    /***
+     * ^ - begin, exclude ' '(space)
+     * ( ) - group
+     * "*" - 0 or more matches
+     * $ - end, exclude ' '(space)
+     */
+    
     public static Regex MultipleOf3() {
         // Regular expression that matches binary inputs that are multiple of 3
-        return new Regex("");
+        return new Regex("^(0*(1(01*0)*1)*)*$");
     }
 }
